adb51fbc4acfa4999672a6c81772d0b82552064a
Ajay Anand <ajayanand10@gmail.com>
2015-12-01 19:55:02 -0800

    final changes (hopefully)

--- mysite.py
+++ mysite.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,5 +7,4 @@ def index():
     return render_template('website.html')
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port=80, debug=False)
-
+    app.run(host = '0.0.0.0', port=80, debug=True)
